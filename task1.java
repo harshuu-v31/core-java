f48a5892e7b780eab226369dc929184c47d46c16
harshuu-v31 <vighneharshada31@gmail.com>
2025-06-29 12:53:58 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,14 @@
+public class task1 {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println("Number=" + n);
+
+        n++;
+        System.out.println("After Increment=" + n);
+
+        n--;
+        System.out.println("After Decrement=" + n);
+        
+    }
+    
+}
