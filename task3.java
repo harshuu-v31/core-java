f3ebb3865bbf5e9f5eb8a6f4d23e94f3c45043cd
harshuu-v31 <vighneharshada31@gmail.com>
2025-06-29 21:45:15 +0530

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,16 @@
+
+
+public class task3 {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number % 2 == 0) {
+            System.out.println("The number " + number + " is Even");
+        } else {
+            System.out.println("The number " + number + " is Odd");
+        }
+    }
+}
+
+
+
