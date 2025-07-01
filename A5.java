ec635ef231000eaead8c84a234f4822ec6648f59
harshuu-v31 <vighneharshada31@gmail.com>
2025-07-01 17:14:05 +0530

    Add files via upload

---
+++ A5.java
@@ -0,0 +1,7 @@
+public class A5 {
+    public static void main(String[] args) {
+        short myNum = 5000;
+        System.out.println("Value: " + myNum);
+        System.out.println("Data Type: " + ((Object)myNum).getClass().getSimpleName());
+    }
+}
